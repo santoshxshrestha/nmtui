b7bfa1297ee8ca09c4c21ff8094a03938ca1099a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-27 18:40:41 +0545

    used app in the tui mod

--- src/utils/tui.rs
+++ src/utils/tui.rs
@@ -1,3 +1,4 @@
+use crate::apps::core::App;
 pub fn tui() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = ratatui::init();
     let app_result = App::default().run(&mut terminal);
