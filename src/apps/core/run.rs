12f06a81b6bbeaaa4efda9211cebd62665a67171
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-29 19:54:13 +0545

    handlers: added the handler to work if the popup to list the saved connection is active

--- src/apps/core/run.rs
+++ src/apps/core/run.rs
@@ -12,6 +12,10 @@ impl App {
             if self.show_help {
                 self.handle_help()?;
             }
+            // to handle the saved connections popup
+            else if self.show_saved {
+                self.handle_saved()?;
+            }
             // to handle the delete confirmation popup
             else if self.show_delete_confirmation {
                 self.handle_delete_confirmation()?;
