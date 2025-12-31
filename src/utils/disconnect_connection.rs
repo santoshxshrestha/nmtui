db184cb0f0639206a7de9c99ea3f0635e21c56ab
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-31 23:35:25 +0545

    add: added the helper function that will disconnect the connected network

--- src/utils/disconnect_connection.rs
+++ src/utils/disconnect_connection.rs
@@ -1,6 +1,37 @@
-use crate::WifiNetwork;
-use std::sync::{Arc, Mutex};
+use crate::{WifiNetwork, apps::handlers::status::Status};
+use std::{
+    process::{Command, ExitStatus},
+    sync::{Arc, Mutex},
+};
 
-pub fn disconnect_connected_network(wifi_list: Arc<Mutex<Vec<WifiNetwork>>>) {
-    todo!("write logic to disconnect the connection");
+pub fn disconnect_connected_network(wifi_list: Arc<Mutex<Vec<WifiNetwork>>>) -> Status {
+    let list = wifi_list.lock().unwrap();
+
+    for network in list.iter() {
+        if network.in_use {
+            let ssid = &network.ssid;
+            let output = Command::new("nmtui")
+                .args(["connection", "down", ssid])
+                .output();
+            match output {
+                Ok(output) => {
+                    let status = output.status;
+                    if status.success() {
+                        let stdout = String::from_utf8_lossy(&output.stdout);
+                        return Status::new(stdout.into(), status);
+                    } else {
+                        let stderr = String::from_utf8_lossy(&output.stderr);
+                        return Status::new(stderr.into(), status);
+                    }
+                }
+                Err(e) => {
+                    return Status::new(
+                        format!("Failed to execute nmcli: {}", e),
+                        ExitStatus::default(),
+                    );
+                }
+            }
+        }
+    }
+    return Status::new(format!("No connection found"), ExitStatus::default());
 }
