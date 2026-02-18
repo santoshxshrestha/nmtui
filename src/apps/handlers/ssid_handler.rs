3e14fe551e6d11bfe4810a086841941549cd4810
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-18 21:49:33 +0545

    refacto: update ssid handler keymaps

--- src/apps/handlers/ssid_handler.rs
+++ src/apps/handlers/ssid_handler.rs
@@ -6,52 +6,41 @@ use std::time::Duration;
 
 impl WifiInputState {
     pub fn handle_ssid_input(&mut self) -> io::Result<()> {
-        if poll(Duration::from_micros(1))? {
-            match event::read()? {
-                Event::Key(KeyEvent {
-                    code: KeyCode::Left,
-                    ..
-                }) => {
-                    self.move_cursor_left();
-                }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Right,
-                    ..
-                }) => {
-                    move_cursor_right(&self.ssid, &mut self.cursor_pos);
-                }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Esc, ..
-                }) => {
+        if poll(Duration::from_micros(1))?
+            && let Event::Key(KeyEvent { code, .. }) = event::read()?
+        {
+            match code {
+                KeyCode::Esc => {
                     self.flags.show_ssid_popup = false;
+                    return Ok(());
                 }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char(c),
-                    ..
-                }) => {
+                KeyCode::Char(c) => {
                     enter_char(&mut self.ssid, c, &self.cursor_pos);
                     move_cursor_right(&self.ssid, &mut self.cursor_pos);
+                    return Ok(());
                 }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Backspace,
-                    ..
-                }) => {
+                KeyCode::Backspace => {
                     delete_char(&mut self.ssid, &mut self.cursor_pos);
                     self.move_cursor_left();
+                    return Ok(());
                 }
-                Event::Key(KeyEvent {
-                    code: KeyCode::Enter,
-                    ..
-                }) => {
+                KeyCode::Enter => {
                     // when ssid is entered, we should show the password popup
                     // but if the user had entered a password before, we should keep it
                     // so that the user can go back and forth without losing the password
                     self.flags.show_ssid_popup = false;
                     self.flags.show_password_popup = true;
                     self.cursor_pos = self.password.chars().count() as u16;
+                    return Ok(());
+                }
+                KeyCode::Left => {
+                    self.move_cursor_left();
+                }
+                KeyCode::Right => {
+                    move_cursor_right(&self.ssid, &mut self.cursor_pos);
                 }
                 _ => {}
-            };
+            }
         }
         Ok(())
     }
